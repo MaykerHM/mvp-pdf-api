2feb076b8e60af145a3662808d56c57ab015fac7
MaykerHM <hydeki8@gmail.com>
2022-05-10 23:00:55 -0300

    feat: merge-front-back

--- src/controllers/merge-front-back.ts
+++ src/controllers/merge-front-back.ts
@@ -1,9 +1,46 @@
 import { Request, Response } from 'express'
+import { PDFDocument } from 'pdf-lib'
+import fs from 'fs'
+import { promisify } from 'util'
+import { Readable } from 'stream'
 
 export default class MergeFrontBack {
   async handle(req: Request, res: Response) {
     try {
-      return res.send('merge-front-back')
+      const files = req.files as Express.Multer.File[]
+      const mergedPdf = await PDFDocument.create()
+      const unlinkAsync = promisify(fs.unlink)
+
+      const pdfFront = await PDFDocument.load(fs.readFileSync(files[0].path))
+      const pdfBack = await PDFDocument.load(fs.readFileSync(files[1].path))
+      const copiedFrontPages = await mergedPdf.copyPages(
+        pdfFront,
+        pdfFront.getPageIndices()
+      )
+      const copiedBackPages = await mergedPdf.copyPages(
+        pdfBack,
+        pdfBack.getPageIndices()
+      )
+
+      const sheetsAmount = copiedFrontPages.length
+
+      for (let sheetNumber = 0; sheetNumber < sheetsAmount; sheetNumber++) {
+        mergedPdf.addPage(copiedFrontPages[sheetNumber])
+        mergedPdf.addPage(copiedBackPages[sheetNumber])
+      }
+
+      await unlinkAsync(files[0].path)
+      await unlinkAsync(files[1].path)
+
+      const mergedPdfFile = await mergedPdf.save({
+        updateFieldAppearances: true,
+      })
+      res.setHeader('Content-Disposition', 'inline; filename="mergedPDF.pdf"')
+      res.setHeader('Content-Type', 'application/pdf')
+      let stream = new Readable()
+      stream.push(mergedPdfFile)
+      stream.push(null)
+      stream.pipe(res)
     } catch (error) {
       console.log(error)
     }
